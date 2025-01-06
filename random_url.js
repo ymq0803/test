384d2d203a8dde238e422790c90619aff4af1545
ymq0803 <ymq0803@gmail.com>
2025-01-06 22:06:30 +0800

    Update random_url.js

--- random_url.js
+++ random_url.js
@@ -1,4 +1,4 @@
 var url = new Array(); 
-url[0] = 'https://www.amazon.co.jp/';
-url[1] = 'https://www.yahoo.co.jp/'; 
+url[0] = 'https://docs.google.com/forms/d/e/1FAIpQLSc1i2MlPP0bidgxXHlud2y51XyoLPu2Z8jtOB_KzdU9sJbfhg/viewform';
+url[1] = 'https://docs.google.com/forms/d/e/1FAIpQLSdaa4cTZRTksPHYMD-ZdKqihSUrzM2orBOOoXA2AsB2hPUMdQ/viewform'; 
 location.assign(url[Math.round(Math.random()*(url.length-1))]);
